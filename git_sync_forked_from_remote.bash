2cbb197c4828fd0a665c9b1d2340522b0ea369e6
valschmidt <vschmidt@ccom.unh.edu>
2019-02-13 16:02:56 -0800

    Added script to pull all upstream repos and push them to their forked equivalents.

---
+++ git_sync_forked_from_remote.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+for d in ../catkin_ws/src/*; do
+    if [[ -d $d ]]; then
+        if [[ -d $d/.git ]]; then
+            echo
+            echo "*** $d ***"
+            echo
+            cd $d
+            git pull upstream master
+            git push
+            cd ../../../scripts
+        fi
+    fi
+done
+
+echo
+echo "*** documentation ***"
+echo
+cd ../documentation
+git pull upstream master
+git push
+cd ../scripts
+
+
+echo
+echo "*** scripts ***"
+echo
+git pull upstream master
+git push
