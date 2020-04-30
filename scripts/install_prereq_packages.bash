f6ae924ba0fd59cad5a7d988901b735e7102f7fa
valschmidt <vschmidt@ccom.unh.edu>
2020-04-30 10:08:43 -0700

    Added ros-melodic-tf2-geometry_msgs package to meet dependancies.

--- scripts/install_prereq_packages.bash
+++ scripts/install_prereq_packages.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-apt install ros-melodic-geographic-msgs ros-melodic-geodesy ros-melodic-pid ros-melodic-joy ros-melodic-mavros ros-melodic-rosmon python-gdal qtpositioning5-dev libqt5svg5-dev libgeos++-dev ros-melodic-image-transport ros-melodic-tf2-ros
+apt install ros-melodic-geographic-msgs ros-melodic-geodesy ros-melodic-pid ros-melodic-joy ros-melodic-mavros ros-melodic-rosmon python-gdal qtpositioning5-dev libqt5svg5-dev libgeos++-dev ros-melodic-image-transport ros-melodic-tf2-ros ros-melodic-tf2-geometry-msgs
